b6b47ea699608f077f583adc566bd95fa9794253
TAKE <TAKE@TAKE-PC>
2015-07-31 17:34:31 +0900

    testtestaaa

--- src/tutorial/main.java
+++ src/tutorial/main.java
@@ -2,6 +2,6 @@ package tutorial;
 
 public class main {
 	public static void main(String[] args) {
-		System.out.println("this is a test by takebayshi15. push test.");
+		System.out.println("this is a test by takebayshi15. push test. pushtesttest");
 	}
 }
