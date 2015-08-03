012a21f478e4def30d4271f277c89e364ec99015
TakayukiSasaki <TakayukiSasaki@192.168.1.129>
2015-08-03 14:34:57 +0900

    double

--- src/tutorial/main.java
+++ src/tutorial/main.java
@@ -5,9 +5,9 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("this is a test by takebayshi15. push test.");
 		System.out.println("this is tested by Aomatsu.");
-		int a = 51;
-		int b = 33;
-		int sum = a + b;
+		double a = 51.4;
+		double b = 33.4;
+		double sum = a + b;
 		System.out.println("sum="+sum);
 //あああああああああああああいえうおんんんんんんんんんんんんんっんんんん
 		///uahfkjnadiuvhiuanfk
