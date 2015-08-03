974ccc8099d48d6b5ff164d8631ffbe74d32d4cd
Aomatsu <Kuesy@192.168.1.121>
2015-08-03 14:06:13 +0900

    okurimasuta

--- src/tutorial/main.java
+++ src/tutorial/main.java
@@ -4,6 +4,11 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("this is a test by takebayshi15. push test.");
 		System.out.println("this is tested by Aomatsu.");
+		int a = 51;
+		int b = 33;
+		int sum = a + b;
+		System.out.println("sum="+sum);
+		
 		
 	}
 }
